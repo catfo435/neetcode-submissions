fbc172970037f37cba56c807a40a45c76498aa63
Pranav Poluri <74195851+catfo435@users.noreply.github.com>
2026-09-02 23:21:35 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(char c){
+        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+    }
+    bool isPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+
+        while (l < r){
+            while (l < r && !isValid(s[l])) l++;
+            while (l < r && !isValid(s[r])) r--;
+
+            if (tolower(s[l]) != tolower(s[r])) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
